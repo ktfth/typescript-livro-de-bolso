95298c9ca2803c6d54d0f8b51a1d0843860ddcad
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-10-28 11:06:06 -0300

    The application of examples

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+const assert = require('assert');
+
+function search(term, text) {
+  return (new RegExp(term)).test(text);
+}
+assert.ok(search('foo', 'foobar'));
+
+function times(term, text) {
+  return text.match((new RegExp(term, 'g'))).length;
+}
+assert.equal(times('baz', 'bazfoobarbaz'), 2);
